1ae5a071641bdaad7362cdbba685933959a65391
natalia_kurthan <kurthannatalia@gmail.com>
2024-11-01 01:30:55 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import pkg from 'glob';
+const { glob } = pkg;
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
